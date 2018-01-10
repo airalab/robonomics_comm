a7d999bb4e6ea0867715d85f362c7f3c2447202f
Alexander Krupenkin <mail@akru.me>
2018-01-10 13:34:24 +0300

    Simple liability finish condition

--- robonomics_liability/src/robonomics_liability/executor.py
+++ robonomics_liability/src/robonomics_liability/executor.py
@@ -6,7 +6,6 @@
 from robonomics_liability.msg import Liability
 from std_srvs.srv import Empty, EmptyResponse
 from tempfile import TemporaryDirectory 
-from threading import Thread, Condition
 from web3 import Web3, HTTPProvider
 from urllib.parse import urlparse
 from .recorder import Recorder
@@ -16,7 +15,7 @@ import rospy, ipfsapi, os
 
 class Executor:
     liability_queue = Queue()
-    liability_finish = Condition()
+    liability_finish = False
 
     def __init__(self):
         '''
@@ -40,9 +39,8 @@ class Executor:
         rospy.Subscriber('incoming', Liability, incoming_liability)
 
         def finish_liability(msg):
-            with self.liability_finish as finish:
-                finish.notify()
-                return EmptyResponse()
+            self.liability_finish = True
+            return EmptyResponse()
         rospy.Service('finish', Empty, finish_liability)
 
         self.complete = rospy.Publisher('complete', Liability, queue_size=10)
@@ -53,32 +51,33 @@ class Executor:
             msg = self.liability_queue.get()
             rospy.loginfo('Start work on liability %s', msg.address)
             self.current.publish(msg)
+            self.liability_finish = False
 
-            with self.liability_finish as finish:
-                with TemporaryDirectory() as tmpdir:
-                    rospy.logdebug('Temporary directory created: %s', tmpdir)
-                    os.chdir(tmpdir)
+            with TemporaryDirectory() as tmpdir:
+                rospy.logdebug('Temporary directory created: %s', tmpdir)
+                os.chdir(tmpdir)
 
-                    rospy.logdebug('Getting objective %s...', msg.objective)
-                    self.ipfs.get(msg.objective)
-                    rospy.logdebug('Objective is written to %s', tmpdir + '/' + msg.objective)
+                rospy.logdebug('Getting objective %s...', msg.objective)
+                self.ipfs.get(msg.objective)
+                rospy.logdebug('Objective is written to %s', tmpdir + '/' + msg.objective)
 
-                    result_file = os.path.join(tmpdir, 'result.bag')
-                    rospy.logdebug('Start recording to %s...', result_file)
+                result_file = os.path.join(tmpdir, 'result.bag')
+                rospy.logdebug('Start recording to %s...', result_file)
 
-                    recorder = Recorder(result_file)
-                    recorder.start()
-                    rospy.logdebug('Rosbag recorder started')
+                recorder = Recorder(result_file)
+                recorder.start()
+                rospy.logdebug('Rosbag recorder started')
 
-                    player = Player(msg.objective)
-                    player.start()
-                    rospy.logdebug('Rosbag player started')
+                player = Player(msg.objective)
+                player.start()
+                rospy.logdebug('Rosbag player started')
 
-                    finish.wait()
+                while not self.liability_finish:
+                    rospy.sleep(1)
 
-                    msg.result = self.ipfs.add(result_file)['Hash']
-                    self.complete.publish(msg)
-                    rospy.loginfo('Liability %s finished with %s', msg.address, msg.result)
+                msg.result = self.ipfs.add(result_file)['Hash']
+                self.complete.publish(msg)
+                rospy.loginfo('Liability %s finished with %s', msg.address, msg.result)
 
     def spin(self):
         '''
