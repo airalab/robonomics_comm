370ced360197c1f097464c4e177e75cc7e82ee13
strdn <eilyichev@aira.life>
2018-10-08 14:30:59 +0300

    pubsub: improvements of works with topics

--- robonomics_lighthouse/src/robonomics_lighthouse/pubsub.py
+++ robonomics_lighthouse/src/robonomics_lighthouse/pubsub.py
@@ -4,7 +4,7 @@
 #
 # Notice: ipfs daemon with `--enable-pubsub-experiment` should be started.
 
-from base64 import b64encode, b64decode
+from base64 import b64decode
 from json import dumps, loads
 import rospy
 
@@ -12,17 +12,16 @@ def publish(ipfs_client, topic, msg):
     '''
         Publish message to given topic.
     '''
-    msgdata = b64encode(dumps(msg).encode('utf-8')).decode('utf-8').join('\r\n')
-    return ipfs_client.pubsub_pub(topic, msgdata)
+    return ipfs_client.pubsub_pub(topic, dumps(msg))
 
 def subscribe(ipfs_client, topic):
     '''
         Subscribe to given topic and return generator of received messages.
     '''
     with ipfs_client.pubsub_sub(topic) as sub:
-        while sub:
+        for msg in sub:
             try:
-                yield loads(b64decode(b64decode(sub.read_message()['data'])).decode('utf-8'))
+                yield loads(b64decode(msg['data']).decode('utf-8'))
             except Exception as e:
                 rospy.logerr(e)
                 rospy.sleep(1)
