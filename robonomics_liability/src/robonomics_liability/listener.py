646b1d5e326cca120f918b48c551460948554210
Alexander Krupenkin <mail@akru.me>
2018-11-10 02:34:10 +0300

    Fix base58 bug in ubuntu

--- robonomics_liability/src/robonomics_liability/listener.py
+++ robonomics_liability/src/robonomics_liability/listener.py
@@ -84,8 +84,8 @@ class Listener:
         c = self.web3.eth.contract(address, abi=self.liability_abi)
         msg = Liability()
         msg.address = address
-        msg.model = b58encode(c.call().model())
-        msg.objective = b58encode(c.call().objective())
+        msg.model = b58encode(c.call().model()).decode()
+        msg.objective = b58encode(c.call().objective()).decode()
         msg.promisee = c.call().promisee()
         msg.promisor = c.call().promisor()
         msg.token = c.call().token()
