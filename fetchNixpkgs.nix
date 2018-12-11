a360eb91691eaace31602ad5022b39e02756a483
Alexander Krupenkin <mail@akru.me>
2018-12-11 11:26:55 +0300

    Bump airapkgs revision

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
-{ rev    ? "812f01d8ef840b2d5a11e6e84872f66f0e86ec54"             # The Git revision of nixpkgs to fetch
-, sha256 ? "1n43js09fx9jav27652mkmkkxh01675wnv8bj1mnj4p62cwricsf" # The SHA256 of the downloaded data
+{ rev    ? "282e60c4f304a8def25f3d031305d1637434f9ba"             # The Git revision of nixpkgs to fetch
+, sha256 ? "1h20malr7ghcaqfql6fgf72dkgkclqpxix4d0qbrpjm44nsz1f48" # The SHA256 of the downloaded data
 , system ? builtins.currentSystem                                 # This is overridable if necessary
 }:
 
