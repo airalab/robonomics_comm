5dd2ee4ecf2d560fe45e7a9f655fcd273fe8100c
Alexander Krupenkin <mail@akru.me>
2019-04-30 14:30:27 +0300

    Fixed airapkgs revision

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
-{ rev    ? "2aa81442f00bd287af3d883feb0d69578bfa7df1"             # The Git revision of nixpkgs to fetch
-, sha256 ? "0c19ym0fwpj8jg8dmrgyfvcca68qmlmfigpp8ghw5yfg2kpbqmh4" # The SHA256 of the downloaded data
+{ rev    ? "e760716c65e7223c5d3dd4ac8a629504b387e0d1"             # The Git revision of nixpkgs to fetch
+, sha256 ? "0wir84c90n78kc51hpg3ps422p9wh479c62sq9201l0i80lih3l7" # The SHA256 of the downloaded data
 }:
 
 builtins.fetchTarball {
