64a439829cff80dca0d707f746ce95ea8c08638d
Alexander Krupenkin <mail@akru.me>
2019-05-23 16:19:08 +0300

    Bump versions

--- fetchNixpkgs.nix
+++ fetchNixpkgs.nix
@@ -1,5 +1,5 @@
-{ rev    ? "e760716c65e7223c5d3dd4ac8a629504b387e0d1"             # The Git revision of nixpkgs to fetch
-, sha256 ? "0wir84c90n78kc51hpg3ps422p9wh479c62sq9201l0i80lih3l7" # The SHA256 of the downloaded data
+{ rev    ? "e8ea32f3a1ccf19bce6c7e20678b0a308d4731b6"             # The Git revision of nixpkgs to fetch
+, sha256 ? "12z0ndyhdzymc4adlsp00h5543n32gnwagaxw6fc2kwrmhqgafln" # The SHA256 of the downloaded data
 }:
 
 builtins.fetchTarball {
